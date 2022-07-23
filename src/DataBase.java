a342d15e042deeb6e405c08866783c74d1950817
PIV-y <iv.proshin@yandex.ru>
2022-07-23 14:04:56 +0400

    Commit 4 Continue Reorganizing classes.

--- src/DataBase.java
+++ src/DataBase.java
@@ -2,17 +2,44 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DataBase {
-    public static void db_connect(String dbTbl, String Indx, String measure){
+    double parameter;
+    public static void db_connect(String dbTbl, String Indx, String measure, String query2user){
         String query = "select * from " + dbTbl;
         try {
             DB_srvINFO.setCon(DriverManager.getConnection(DB_srvINFO.url, DB_srvINFO.user, DB_srvINFO.password));
             DB_srvINFO.setStmt(DB_srvINFO.getCon().createStatement());
             DB_srvINFO.setRs(DB_srvINFO.getStmt().executeQuery(query));
+            System.out.println(query2user);
             while (DB_srvINFO.getRs().next()) {
                 double id_Name = DB_srvINFO.getRs().getDouble(1);
                 double parameter = DB_srvINFO.getRs().getDouble(2);
                 System.out.println(Indx + id_Name + " = " + parameter + measure);
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {DB_srvINFO.getCon().close();} catch (SQLException e) {e.printStackTrace();}
+            try {DB_srvINFO.getStmt().close();} catch (SQLException e) {e.printStackTrace();}
+            try {DB_srvINFO.getRs().close();} catch (SQLException e) {e.printStackTrace();}
+        }
+    }
+
+    public static void db_fetch(String dbTbl, String id_Name) {
+        String query = "select parameter from " + dbTbl + " where id_Name " + id_Name;
+        UserDataCollection userDataCollection = new UserDataCollection();
+       // double parameterConcrete;
+        try {
+            DB_srvINFO.setCon(DriverManager.getConnection(DB_srvINFO.url, DB_srvINFO.user, DB_srvINFO.password));
+            DB_srvINFO.setStmt(DB_srvINFO.getCon().createStatement());
+            DB_srvINFO.setRs(DB_srvINFO.getStmt().executeQuery(query));
+            userDataCollection.RainFrsClass = DB_srvINFO.getRs().getDouble(1);
+/*
+            while (DB_srvINFO.getRs().next()) {
+                double id_Name = DB_srvINFO.getRs().getDouble(1);
+                double parameter = DB_srvINFO.getRs().getDouble(2);
+                System.out.println(Indx + id_Name + " = " + parameter + measure);
+*/
         } catch (SQLException e) {
             e.printStackTrace();
         }
--- src/UserDataCollection.java
+++ src/UserDataCollection.java
@@ -11,35 +11,41 @@ public class UserDataCollection {
 
     public void SetValRainFrsDiameterEst(){
         System.out.println("Диаметр расчетной арматуры, см: ");
-        RainFrsDiameterEst = console.nextInt(); //присвоение значения диаметра
+        RainFrsDiameterEst = console.nextInt();
+        //присвоение значения диаметра расчетной арматуры
     }
 
     public void SetValRainFrsDiameterFact(){
         System.out.println("Диаметр фактически применяемой арматуры, см: ");
-        RainFrsDiameterFact = console.nextInt(); //присвоение значения диаметра
+        RainFrsDiameterFact = console.nextInt();
+        //присвоение значения диаметра фактически используемой арматуры
     }
 
     public void SetValRainFrsClass(){
         String dbTbl = "rainforcement";
         String IndxParameter = "A";
         String measure = "MPa (Rs)";
-        UserDataCollection userDataCollection = new UserDataCollection();
-        System.out.println("Выбери класс арматуры:");
-        DataBase.db_connect(dbTbl, IndxParameter, measure);
-
+        String query2user = "Выбери класс арматуры:";
+        DataBase.db_connect(dbTbl, IndxParameter, measure,query2user);
+        String IDrainforcement = console.nextLine();
+    //    DataBase.db_fetch(dbTbl, IDrainforcement);
+    //    System.out.println(RainFrsClass);
     }
 
+
     public void SetValConcreteClass(){
+/*
         String dbTbl = "concrete";
         String IndxParameter = "B";
         String measure = "MPa (Rbond)";
         UserDataCollection userDataCollection = new UserDataCollection();
         System.out.println("Выбери класс бетонна:");
         DataBase.db_connect(dbTbl, IndxParameter, measure);
-
-
-
+*/
     }
+
+
+
 
     public void SetValWorkTypeKf(){
 
