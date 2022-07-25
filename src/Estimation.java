2ab4b6e37f25ae779d5f9251d89be9e830dc31f3
PIV-y <iv.proshin@yandex.ru>
2022-07-25 22:36:26 +0400

    Commit 5 Finished

--- src/Estimation.java
+++ src/Estimation.java
@@ -1,25 +1,24 @@
 import javax.xml.crypto.Data;
 
 public class Estimation {
-    public static void main(String[] args) {
+    public static double BaseAnchorLength;
+    public static double MinAnchorLength;
 
+    public static void main(String[] args) {
         UserDataCollection.SetValReinFrsDiameterEst();
         UserDataCollection.SetValReinFrsDiameterFact();
         UserDataCollection.SetValReinFrsClass();
         UserDataCollection.SetValConcreteClass();
         UserDataCollection.SetValWorkTypeKf();
         EstDataCollection.SetValSectionPerimeter();
+        Calculation();
+    }
 
-/*
-        double BaseAnchorLength;
-        double MinAnchorLength;
-        DataCollection Data = new DataCollection();
-        Data.SetVal();
-
-        BaseAnchorLength = (Data.ReinforceType*Data.ReinforceSectionAreaEst) / (
-                (2.5*Data.ConcreteType) * Data.ReinforcePerimeterSectionEst);
-        MinAnchorLength = Data.WorkTypeKf * BaseAnchorLength * (Data.ReinforceSectionAreaEst/Data.LngthSectionAreaFact);
-        System.out.println("Длинна анкеровки: " + MinAnchorLength + "мм");
-*/
+    public static void Calculation(){
+        BaseAnchorLength = (UserDataCollection.ReinFrsClass*EstDataCollection.ReinFrsSectionAreaEst) / (
+                (2.5*UserDataCollection.ConcreteClass)*EstDataCollection.ReinFrsPerimeterSectionEst);
+        MinAnchorLength = UserDataCollection.WorkTypeKf * BaseAnchorLength * (
+                EstDataCollection.ReinFrsSectionAreaEst/EstDataCollection.ReinFrsSectionAreaFact);
+        System.out.println("Минимальная длинна анкеровки: " + MinAnchorLength + "см");
     }
 }
